3fe29f716eaf0c4b8485aff68aa482bef3cdcfe2
Noé Poirier <noer5327@gmail.com>
2026-08-26 22:31:43 +0200

    Ajout d'option de fonctionnement pour glfw.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@ int main() {
     return -1;
   }
 
+  // Option de fonctionnement du GLFW
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
   window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
   glfwMakeContextCurrent(window);
 
