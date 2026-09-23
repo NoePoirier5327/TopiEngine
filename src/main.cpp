c7dfe809effbd41c32c053de61cb43fb1b5777f0
Noé Poirier <noer5327@gmail.com>
2026-09-23 10:03:01 +0200

    Test des nouveaux constructeurs.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
   TopiEngine topi = TopiEngine("Test", 400, 800);
 
-  MultilayerColoredTilemap tilemap = MultilayerColoredTilemap(10, 20, 2, 32, 32);
+  MultilayerColoredTilemap tilemap = MultilayerColoredTilemap(10, 20, 2);
   tilemap.new_tile(0, 255, 100, 100, 255);
   tilemap.new_tile(1, 100, 255, 100, 255);
   tilemap.new_tile(2, 100, 100, 255, 255);
