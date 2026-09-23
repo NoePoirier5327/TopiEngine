a6b2c0bcf7e162344a0c20626d00cad8ea2f4969
Noé Poirier <noer5327@gmail.com>
2026-09-23 12:39:53 +0200

    Teste de la fonctionnalité de flip.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "topi/input_manager/input_manager.hpp"
 #include "topi/object/tilemap/tilemap.hpp"
 #include "topi/topi.hpp"
 #include <cstdint>
@@ -5,20 +6,21 @@
 int main() {
   TopiEngine topi = TopiEngine("Test", 400, 800);
 
-  MultilayerColoredTilemap tilemap = MultilayerColoredTilemap(10, 20, 2, 32, 0, 32, 32);
+  ColoredTilemap tilemap = ColoredTilemap(10, 20);
   tilemap.new_tile(0, 255, 100, 100, 255);
   tilemap.new_tile(1, 100, 255, 100, 255);
   tilemap.new_tile(2, 100, 100, 255, 255);
 
   for (size_t x = 0; x < 10; ++x) {
     for (size_t y = 0; y < 20; ++y) {
-      tilemap(x, y, 0) = static_cast<uint64_t>((x + y) % 3);
-      tilemap(x, y, 1) = 2;
+      tilemap(x, y) = static_cast<uint64_t>((x + y) % 3);
     }
   }
 
+  tilemap.flip_y();
+
   topi.on_command().on_display([&tilemap](Renderer *renderer) {
-      tilemap.display(renderer, 0);
+    tilemap.display(renderer);
   });
 
   topi.run();
